f4636eda0f3a43c74d7a9f7ee2aeef0abe3b62e6
Anthony shannon <anthonyshannon@Jacobs-MBP-3.localdomain>
2019-02-05 16:03:31 -0500

    added redux folders and imports to indjex.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
 import './App.css';
-import Homepage from './pages/Homepage.jsx'
-import CreateOffering from './pages/CreateOffering.jsx'
-import LoginPage from './pages/Login.jsx'
-import SignupPage from './pages/Signup.jsx'
-import MenuAppBar from './components/TopAppBar'
+import Homepage from './pages/Homepage.jsx';
+import CreateOffering from './pages/CreateOffering.jsx';
+import LoginPage from './pages/Login.jsx';
+import SignupPage from './pages/Signup.jsx';
+import MenuAppBar from './components/TopAppBar';
 import { object } from 'prop-types';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import green from '@material-ui/core/colors/green'
+import green from '@material-ui/core/colors/green';
 
 
 const theme = createMuiTheme({
